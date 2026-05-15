f358df20acb193372ecdf63ee3255fddb5e2859c
mahnooor786 <mahnooor71@gmail.com>
2026-05-15 01:54:43 -0800

    Add files via upload

--- database.sql
+++ database.sql
@@ -1,10 +1,58 @@
-CREATE DATABASE hospital_db;
-USE hospital_db;
+CREATE DATABASE hospital_management;
+
+USE hospital_management;
+
+-- PATIENT TABLE
 
 CREATE TABLE patients (
-    id INT PRIMARY KEY,
-    name VARCHAR(100),
+
+    patient_id INT PRIMARY KEY AUTO_INCREMENT,
+
+    patient_name VARCHAR(100),
+
     age INT,
-    gender VARCHAR(10),
-    date_added TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+
+    gender VARCHAR(20),
+
+    disease VARCHAR(100)
+
 );
+
+-- DOCTOR TABLE
+
+CREATE TABLE doctors (
+
+    doctor_id INT PRIMARY KEY AUTO_INCREMENT,
+
+    doctor_name VARCHAR(100),
+
+    specialization VARCHAR(100)
+
+);
+
+-- APPOINTMENT TABLE
+
+CREATE TABLE appointments (
+
+    appointment_id INT PRIMARY KEY AUTO_INCREMENT,
+
+    patient_name VARCHAR(100),
+
+    doctor_name VARCHAR(100),
+
+    appointment_date DATE
+
+);
+
+-- INSERT SAMPLE DOCTORS
+
+INSERT INTO doctors
+(doctor_name, specialization)
+
+VALUES
+
+('Dr. Ali', 'Cardiologist'),
+
+('Dr. Sara', 'Dentist'),
+
+('Dr. Ahmed', 'General Physician');
